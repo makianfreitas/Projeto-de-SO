b09c7637c30917e32633d5152bf2af06ac4ca6aa
Makian Freitas Costa <makianfaculdade@gmail.com>
2023-06-08 22:06:49 -0300

    Adicionei os comentários
    Adicionei os comentários explicando pra que cada coisa serve

--- projeto/tarefaMaisCurtaPreemptiva.c
+++ projeto/tarefaMaisCurtaPreemptiva.c
@@ -14,115 +14,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
 
 typedef struct strcProcesso{
-	int tamJobs;
-	int idProcesso; 
-	int faltaProcessar;
-	int tempoChegadaNaFila;
+	int tamJobs; //Tamanho do processo
+	int idProcesso; //Armazena um código int que será impresso como char (%c) para mostrar uma letra (A, B, C...)
+	int faltaProcessar; //Quantidade que falta processar
+	int tempoChegadaNaFila; //Tempo de chegada do processo
 }processo;
 
-void criaProcessos(processo *vetProc, int *vetTempoChegada);
+void criaProcessos(processo *vetProc);
 void imprimeProcessosCriados(processo *vetProc);
 void function(processo *vetProc, int *fila);
 
-int i, j, k;
-int totProcesso;
+int totProcesso; //Quantidade total de processos e tamanho lógico do vetor principal vetProcessos[]
+/*
+	totProcesso é a posição limite do vetor principal (vetProcessos[]), 
+	ou seja, o vetor será lido até a posição tamFila. Se tiver alguma coisa 
+	amazenada depois desta posição (no vetor vetProcessos[]), será 
+	ignorado pois se trata de processo(s) finalizado(s)
+*/
 
 int main(){
 	system("cls");
 	printf("Digite o numero de processos: ");
 	scanf("%d",&totProcesso);
 
-	processo vetProc[totProcesso];
-	int fila[totProcesso];
-	int vetTempoChegada[totProcesso];
+	processo vetProcessos[totProcesso]; //Vetor (PRINCIPAL) do tipo processo que armazena as características de cada processo
+	int fila[totProcesso]; //Armazena, por ordem de chegada, a posição de cada processo no vetor principal (vetProcessos[])
 	
-	criaProcessos(vetProc, vetTempoChegada);
-	imprimeProcessosCriados(vetProc);
-	function(vetProc, fila);
-	printf("\n\n");
+	criaProcessos(vetProcessos);
+	imprimeProcessosCriados(vetProcessos);
+	system("pause");
+	function(vetProcessos, fila);
+	printf("\t----------------------------------------------------\n\n");
 	system("pause");
 	return 0;
 }
 
-void criaProcessos(processo *vetProc, int *vetTempoChegada) {
+void criaProcessos(processo *vetProcessos) {
 	int cont;
 	srand(time(NULL));
     for (cont = 0; cont < totProcesso; cont++) {
-		vetProc[cont].tamJobs = 4 + rand() % 16; //tamanho do job do processo é gerado aleatoriamente
-		vetProc[cont].tempoChegadaNaFila = rand() % 15; //tempo de chegada é gerado aleatoriamente
-		vetProc[cont].idProcesso = 65 + cont;
-		vetProc[cont].faltaProcessar = vetProc[cont].tamJobs;
+		vetProcessos[cont].tamJobs = 4 + rand() % 16; //Tamanho do processo é gerado aleatoriamente (de 4 a 20)
+		vetProcessos[cont].tempoChegadaNaFila = rand() % 15; //Tempo de chegada é gerado aleatoriamente (de 0 a 15)
+		vetProcessos[cont].idProcesso = 65 + cont; //idProcesso recebe o código de cada letra baseado na tabela ASCII (exemplo: 65 = A e 90 = Z)
+		vetProcessos[cont].faltaProcessar = vetProcessos[cont].tamJobs; //faltaProcessar recebe inicialmente o tamanho do processo (tamJobs)
     }
 }
 
 
-void imprimeProcessosCriados(processo *vetProc) {
+void imprimeProcessosCriados(processo *vetProcessos) {
 	int cont;
-	printf("\tID Processo | Tamanho do Job | Tempo de Chegada\n");
+	system("cls");
+	system("cls");
+	printf("\t----------------------------------------------------\n");
+	printf("\t| ID Processo | Tamanho do Job | Tempo de Chegada  |\n");
 	for(cont = 0; cont < totProcesso; cont++){
-		printf("\t   %c \t    |    %d \t     |    %d \n", vetProc[cont].idProcesso, vetProc[cont].tamJobs, vetProc[cont].tempoChegadaNaFila);
+		printf("\t----------------------------------------------------\n");
+		printf("\t|    %c \t      |    %d \t       |    %d \t\t   |\n", vetProcessos[cont].idProcesso, vetProcessos[cont].tamJobs, vetProcessos[cont].tempoChegadaNaFila);
 	}
-	system("pause");
+	printf("\t----------------------------------------------------\n\n");
 }
 
-void function(processo *vetProc, int *fila){
-	int execultando=-1;
-	int tamFila=-1; //tamFila armazena as posições dos processos
-	system("cls");
-	printf("\t|  Tempo do Processador\t |  Processo em execulcao  |\n");
-	for(i=0; totProcesso > 0; i++){
-		for(j=0; j < totProcesso; j++){
-			if(vetProc[j].tempoChegadaNaFila == i){ //Verifica se chegou processo no tempo atual
+void function(processo *vetProcessos, int *fila){
+	int i, j; //Variáveis para os laços
+	int tempo; //Tempo atual
+	int execultando=-1; //Armazena a posição, no vetor principal (vetProcessos[]), do processo que está em execulção
+	int tamFila=-1; //Tamanho lógico do vetor fila[]
+	/*
+		tamFila é a posição limite do vetor fila[], ou seja, 
+		o vetor será lido até a posição tamFila. Se tiver alguma coisa 
+		amazenada depois desta posição (no vetor fila[]), será 
+		ignorado pois se trata de processo(s) finalizado(s)
+	*/
+
+	imprimeProcessosCriados(vetProcessos);
+	
+	
+	//LEMBRAR DE ADICIONAR A TERCEIRA COLUNA (ignorem)
+	
+	
+	printf("\n\t----------------------------------------------------\n");
+	printf("\t|  Tempo do Processador\t |  Processo em Execulcao  |\n");
+	for(tempo=0; totProcesso > 0; tempo++){ //Laço que representa o tempo
+
+		for(i=0; i < totProcesso; i++){ //Laço que verifica se chegou processo no tempo atual
+			if(vetProcessos[i].tempoChegadaNaFila == tempo){
 				tamFila++; //"Aumenta" o tamanho da fila
-				fila[tamFila] = j; //Adiciona a posição do processo na fila
+				fila[tamFila] = i; //fila[] recebe a posição, no vetor principal (vetProcessos[]), do processo que chegou
 			}
 		}
 		
-		if(tamFila > -1){
+		if(tamFila > -1){ //Verifica se existe processo para ser execultado
 			execultando = fila[0];
-			for(j=1; j <= tamFila; j++){ //Verifica qual é o processo com a menor tarefa
-				if(vetProc[fila[j]].faltaProcessar < vetProc[execultando].faltaProcessar){
-					execultando = fila[j]; //execultando recebe a posição do processo com a menor tarefa
+			for(i=1; i <= tamFila; i++){ //Laço que verifica qual é o processo com o menor tamanho
+				if(vetProcessos[fila[i]].faltaProcessar < vetProcessos[execultando].faltaProcessar){
+					execultando = fila[i]; //execultando recebe a posição, no vetor principal (vetProcessos[]), do processo com o menor tamanho
 				}
 			}
-			printf("\t----------------------------------------------------\n");
-			printf("\t|\t\t  %d\t |\t%c\t\t   |\n", i, vetProc[execultando].idProcesso);
-			vetProc[execultando].faltaProcessar--;
 
-			if(vetProc[execultando].faltaProcessar == 0){
-				for(j=0; j <= tamFila; j++){ //"Tira" o processo da fila
-					if(fila[j] == execultando){ //Verifica se essa posição é o processo que será "apagado" da fila
-						for(k=j; k <= tamFila-1; k++){
-							fila[k] = fila[k+1];
+			printf("\t----------------------------------------------------\n");
+			printf("\t|\t\t  %d\t |\t%c\t\t   |\n", tempo, vetProcessos[execultando].idProcesso); //Execulta
+			vetProcessos[execultando].faltaProcessar--;
+
+			if(vetProcessos[execultando].faltaProcessar == 0){ //Verifica se o processo que acabou de ser execultado terminou
+				for(i=0; i <= tamFila; i++){ //Laço que procura este processo no vetor fila[]
+					if(fila[i] == execultando){ //Verifica se esta posição é o processo que será "apagado" do vetor fila[]
+						for(j=i; j <= tamFila-1; j++){ //Laço que "apaga" o processo do vetor fila[]
+							fila[j] = fila[j+1];
 						}
-						break;
+						break; //Se o processo for encontrado, o laço que procura o processo será interrompido
 					}
 				}
-				for(j=execultando; j < totProcesso-1; j++){ //"Tira" o processo do vetor
-					vetProc[j] = vetProc[j+1];
-				}
-				vetProc[j].idProcesso = 0;
-				vetProc[j].faltaProcessar = 0;
-				
-				for(j=0; j <= tamFila-1; j++){
-					if(fila[j] > execultando){
-						fila[j]--;
-					}
-				}
-				
-				tamFila--;
-				totProcesso--;
-			}
-		} else {
-			printf("\t----------------------------------------------------\n");
-			printf("\t|\t\t  %d\t |\t\t\t   |\n", i);
-		}
 
-		if(i == 1000){
-			system("pause");
+				for(i=execultando; i < totProcesso-1; i++){ //Laço que "apaga" o processo do vetor principal (vetProcessos[])
+					vetProcessos[i] = vetProcessos[i+1];
+				}
+				totProcesso--;
+
+				for(i=0; i <= tamFila-1; i++){ 
+					/*
+						Quando o processo foi "apagado" do vetor principal (vetProcessos[]),
+						todos os processos depois dele deslocaram 1 posição pra trás. Então esse
+						laço serve para subtrair 1 (no vetor fila[]) de todos os números que 
+						forem maiores do que a posição, no vetor principal (vetProcessos[]), 
+						do processo que foi "apagado"
+					*/
+					if(fila[i] > execultando){
+						fila[i]--;
+					}
+				}
+				tamFila--;
+			}
+		} else { //Não tem processo no momento
+			printf("\t----------------------------------------------------\n");
+			printf("\t|\t\t  %d\t |\t\t\t   |\n", tempo);
 		}
 	}
 }
